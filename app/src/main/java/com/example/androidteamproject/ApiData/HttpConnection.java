d45aa318df79be68aa6f3b9276285e9afddef324
woodonghyeon <silhone__@naver.com>
2024-05-19 13:04:06 +0900

    UPDATE : data4Library API

---
+++ app/src/main/java/com/example/androidteamproject/ApiData/HttpConnection.java
@@ -0,0 +1,62 @@
+package com.example.androidteamproject.ApiData;
+
+import android.content.Context;
+
+import com.example.androidteamproject.R;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpConnection {
+
+    private static final String BASE_URL = "http://data4library.kr/api/libSrch";
+    private static String API_KEY;
+
+    private static HttpConnection instance;
+    private OkHttpClient client;
+
+    private HttpConnection(Context context) {
+        client = new OkHttpClient();
+        API_KEY = context.getResources().getString(R.string.api_key);
+    }
+
+    public static HttpConnection getInstance(Context context) {
+        if (instance == null) {
+            instance = new HttpConnection(context);
+        }
+        return instance;
+    }
+
+    public void getLibraries(int pageNo, int pageSize, String format, final HttpResponseCallback callback) {
+        String url = BASE_URL + "?authKey=" + API_KEY + "&pageNo=" + pageNo + "&pageSize=" + pageSize + "&format=" + format;
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                callback.onFailure(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    callback.onSuccess(responseData);
+                } else {
+                    callback.onFailure(new IOException("Unexpected code " + response));
+                }
+            }
+        });
+    }
+
+    public interface HttpResponseCallback {
+        void onSuccess(String responseData);
+        void onFailure(Exception e);
+    }
+}
