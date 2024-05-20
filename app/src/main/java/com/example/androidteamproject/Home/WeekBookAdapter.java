e16b5e21cd6f21667f7d0feeaff39b0ecadbcff3
Woodonghyeon <silhone__@naver.com>
2024-05-20 18:46:17 +0900

    Modify: 이번주 인기도서 Author(작가) 추가

--- app/src/main/java/com/example/androidteamproject/Home/WeekBookAdapter.java
+++ app/src/main/java/com/example/androidteamproject/Home/WeekBookAdapter.java
@@ -12,12 +12,14 @@ import java.util.List;
 public class WeekBookAdapter extends FragmentStateAdapter {
     private final List<String> imageUrls;
     private final List<String> bookName;
+    private final List<String> authors;
     private static final int MAX_VALUE = 2000; // 충분히 큰 값
 
-    public WeekBookAdapter(FragmentActivity fa, List<String> bookName, List<String> imageUrls) {
+    public WeekBookAdapter(FragmentActivity fa, List<String> bookName, List<String> authors, List<String> imageUrls) {
         super(fa);
         this.imageUrls = imageUrls;
         this.bookName = bookName;
+        this.authors = authors;
     }
 
     @NonNull
@@ -25,7 +27,7 @@ public class WeekBookAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         // 이미지 리소스 대신 URL을 전달하여 Fragment를 생성
         int index = position % imageUrls.size(); // 인덱스를 이미지 URL의 개수로 나눈 나머지로 계산
-        return WeekBookImageFragment.newInstance(bookName.get(index), imageUrls.get(index));
+        return WeekBookImageFragment.newInstance(bookName.get(index), authors.get(index), imageUrls.get(index));
     }
 
     @Override
