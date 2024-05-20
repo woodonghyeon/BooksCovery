7a341672ce00e0cdd4675f087831d75a97f712b0
Mo yuseung <ahdbtmd147@naver.com>
2024-05-21 00:18:58 +0900

    DB연동 -> 회원가입, 로그인 (기능만) 구현

--- app/src/main/java/com/example/androidteamproject/Login/LoginActivity.java
+++ app/src/main/java/com/example/androidteamproject/Login/LoginActivity.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.androidteamproject.Home.HomeActivity;
+import com.example.androidteamproject.LoginCheck.LoginCheckActivity;
 import com.example.androidteamproject.Join.JoinActivity;
 import com.example.androidteamproject.R;
 
 public class LoginActivity extends Activity {
+
+    private EditText et_input_id;
+    private EditText et_input_pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +34,18 @@ public class LoginActivity extends Activity {
         bt_login = findViewById(R.id.bt_login);
         Button btJoin = (Button) findViewById(R.id.btJoin);
 
+        //로그인 정보
+        et_input_id = findViewById(R.id.et_input_id);
+        et_input_pwd = findViewById(R.id.et_input_pwd);
+
         // 로그인 버튼 클릭시
-        bt_login.setOnClickListener(view -> { showProgressDialog(); startActivity(new Intent(LoginActivity.this, HomeActivity.class)); });
+        bt_login.setOnClickListener(view -> {
+            showProgressDialog();
+            Intent intent = new Intent(LoginActivity.this, LoginCheckActivity.class);
+            intent.putExtra("ID", et_input_id.getText().toString() );
+            intent.putExtra("PW", et_input_pwd.getText().toString() );
+            startActivityForResult(intent,0);
+        });
 
         // 회원가입 버튼 클릭시
         btJoin.setOnClickListener(view -> { startActivity(new Intent(LoginActivity.this, JoinActivity.class)); });
@@ -42,5 +56,31 @@ public class LoginActivity extends Activity {
         dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         dialog.setMessage("로그인중입니다.");
         dialog.show();
+    }
+
+    //로그인 결과
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //결과가 CANCELED = 로그인에 실패함
+        if(resultCode == RESULT_CANCELED){
+            Toast.makeText(getApplicationContext(),"로그인에 실패했습니다.",Toast.LENGTH_SHORT).show();
+        }
+        //결과가 OK = 로그인이 되었다.
+        else if(resultCode == RESULT_OK){
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            intent.putExtra("Name",data.getStringExtra("Name"));
+            intent.putExtra("Gender",data.getStringExtra("Gender"));
+            intent.putExtra("Age",data.getIntExtra("Age",0));
+            intent.putExtra("Department_id",data.getIntExtra("Department_id",0));
+            intent.putExtra("Id",data.getStringExtra("Id"));
+            intent.putExtra("Pwd",data.getStringExtra("Pwd"));
+            intent.putExtra("Email",data.getStringExtra("Email"));
+            intent.putExtra("Mode",data.getStringExtra("Mode"));
+            intent.putExtra("UpdateDate",data.getStringExtra("UpdateDate"));
+            startActivity(intent);
+        }
+        
     }
 }
