b0fc00951c46abb5f93e94d72c3bd171cf62059e
Mo yuseung <ahdbtmd147@naver.com>
2024-05-20 20:12:05 +0900

    회원가입 버튼 + 창 생성

---
+++ app/src/main/java/com/example/androidteamproject/Join/JoinActivity.java
@@ -0,0 +1,20 @@
+package com.example.androidteamproject.Join;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.androidteamproject.R;
+
+public class JoinActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+
+
+
+
+    }
+}
