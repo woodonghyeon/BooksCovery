5e8a76fb8c714b50c01abec0e8d84b663cc8325d
WooDongHyeon <silhone__@naver.com>
2024-05-21 15:58:44 +0900

    Update : 대출 급상승 도서 추가

--- app/src/main/java/com/example/androidteamproject/ApiData/HttpConnection.java
+++ app/src/main/java/com/example/androidteamproject/ApiData/HttpConnection.java
@@ -195,6 +195,73 @@ public class HttpConnection {
                 }
             }
         });
-    }
+    } // end of LoanItems
 
+    // LoanItems -> 대출 많은 도서를 뽑아옴 후에 수정 예정
+    public void getHotTrend(String searchDt, String format, HttpResponseCallback<List<SearchBook>> callback) {
+        String url = BASE_URL + "loanItemSrch?authKey=" + API_KEY
+                + "&searchDt="
+                + "&format="
+                + format;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                callback.onFailure(e);
+            }
+
+            @Override
+            // HTTP 응답을 처리하는 메서드
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    // 응답이 성공적이지 않은 경우 예외를 던짐
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    // 응답 본문을 JSON 객체로 변환
+                    JSONObject responseBody = new JSONObject(response.body().string());
+
+                    // JSON 객체에서 "response" 객체를 가져와서 "docs" 배열을 추출
+                    JSONArray docs = responseBody.getJSONObject("response").getJSONArray("docs");
+
+                    // SearchBook 객체를 저장할 리스트 초기화
+                    List<SearchBook> books = new ArrayList<>();
+
+                    // docs 배열을 순회하며 각 문서에서 정보를 추출
+                    for (int i = 0; i < docs.length(); i++) {
+                        // 각 문서(doc) 객체를 가져옴
+                        JSONObject doc = docs.getJSONObject(i).getJSONObject("doc");
+
+                        // 주제분류명(class_nm)을 가져옴
+                        String class_nm = doc.getString("class_nm");
+
+                        // 책 이름(bookname)을 가져옴
+                        String bookName = doc.getString("bookname");
+
+                        // 책 이미지 URL(bookImageURL)을 가져옴
+                        String bookImageUrl = doc.getString("bookImageURL");
+
+                        // 저자(authors)를 가져옴
+                        String authors = doc.getString("authors");
+
+                        // 책 정보를 담은 SearchBook 객체 생성
+                        SearchBook book = new SearchBook(class_nm, bookName, authors, bookImageUrl);
+
+                        // 생성한 SearchBook 객체를 리스트에 추가
+                        books.add(book);
+                    }
+
+                    // 콜백을 통해 성공적인 응답 처리 (책 리스트 전달)
+                    callback.onSuccess(books);
+                } catch (JSONException e) {
+                    // JSON 파싱 중 예외가 발생한 경우 콜백을 통해 실패 처리
+                    callback.onFailure(e);
+                }
+            }
+        });
+    } // end of hotTrend
 }
