87f5e74fa308093a417be8ebf4c24bff2ec444d7
박민욱 <parkminuk0203@gmail.com>
2024-05-21 12:55:16 +0900

    UPDATE : 검색 API

--- app/src/main/java/com/example/androidteamproject/Login/LoginActivity.java
+++ app/src/main/java/com/example/androidteamproject/Login/LoginActivity.java
@@ -57,6 +57,7 @@ public class LoginActivity extends Activity {
         dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         dialog.setMessage("로그인중입니다.");
         dialog.show();
+        dialog.dismiss();
     }
 
     //로그인 결과
@@ -82,6 +83,6 @@ public class LoginActivity extends Activity {
             intent.putExtra("UpdateDate",data.getStringExtra("UpdateDate"));
             startActivity(intent);
         }
-        
+
     }
 }
