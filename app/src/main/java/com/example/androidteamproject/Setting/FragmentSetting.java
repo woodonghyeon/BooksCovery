7fb94230db68786d58ea030048db9fdc51c5a400
parkminuk <qkralsdnr0203@naver.com>
2024-05-22 17:16:59 +0900

    UPDATE : 설정 창 로그인 부분

--- app/src/main/java/com/example/androidteamproject/Setting/FragmentSetting.java
+++ app/src/main/java/com/example/androidteamproject/Setting/FragmentSetting.java
@@ -1,5 +1,7 @@
 package com.example.androidteamproject.Setting;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.androidteamproject.R;
 public class FragmentSetting extends Fragment {
@@ -14,6 +18,8 @@ public class FragmentSetting extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    private TextView tv_userid;
+    private Button bt_logout;
     public FragmentSetting() {
         // Required empty public constructor
     }
@@ -39,6 +45,25 @@ public class FragmentSetting extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        Context context = getActivity();
+        if (context != null) {
+            SharedPreferences sharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+            String userid = sharedPreferences.getString("userid", null);
+
+            TextView tv_userid = view.findViewById(R.id.tv_userid);
+            tv_userid.setText(userid);
+            bt_logout = view.findViewById(R.id.bt_logout);
+
+            // 로그아웃 버튼 클릭시
+            bt_logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+
         return view;
     }
 }
