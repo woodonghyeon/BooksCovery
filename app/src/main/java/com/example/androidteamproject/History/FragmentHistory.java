26805a2f09ee2dc7873c5c6b9866a926ab25b3d8
Mo yuseung <ahdbtmd147@naver.com>
2024-05-22 22:15:10 +0900

    기록 fragment 틀만 만들었음

---
+++ app/src/main/java/com/example/androidteamproject/History/FragmentHistory.java
@@ -0,0 +1,51 @@
+package com.example.androidteamproject.History;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.androidteamproject.R;
+import com.example.androidteamproject.Search.FragmentSearch;
+
+public class FragmentHistory extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public static FragmentHistory newInstance(String param1, String param2) {
+        FragmentHistory fragment = new FragmentHistory();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
