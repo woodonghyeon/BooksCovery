bd7037e2c169865502b1b1d39414c24fa283dc52
woodonghyeon <silhone__@naver.com>
2024-05-22 17:18:07 +0900

    Modify : HomeActivity 내비게이션바 replace로 전환 재변경

--- app/src/main/java/com/example/androidteamproject/Home/HomeActivity.java
+++ app/src/main/java/com/example/androidteamproject/Home/HomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -28,17 +29,38 @@ public class HomeActivity extends AppCompatActivity {
         ly_home = findViewById(R.id.ly_home);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        // 초기 홈 프래그먼트 설정
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.ly_home, new FragmentHome());
-        fragmentTransaction.commit();
+        if (savedInstanceState == null) {
+            // 기본 프래그먼트 설정
+            fragmentHome = new FragmentHome();
+            fragmentSearch = new FragmentSearch();
+            fragmentSetting = new FragmentSetting();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.ly_home, fragmentHome, "home")
+                    .commit();
+        } else {
+            fragmentHome = (FragmentHome) getSupportFragmentManager().findFragmentByTag("home");
+            fragmentSearch = (FragmentSearch) getSupportFragmentManager().findFragmentByTag("search");
+            fragmentSetting = (FragmentSetting) getSupportFragmentManager().findFragmentByTag("setting");
+        }
 
         SettingListener();
+
+        // onBackPressedDispatcher를 사용하여 뒤로 가기 콜백 등록
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if (fragmentManager.getBackStackEntryCount() > 0) {
+                    fragmentManager.popBackStack();
+                } else {
+                    finish(); // Activity를 종료합니다.
+                }
+            }
+        });
     }
 
     private void SettingListener() {
-        // 선택 리스너 등록
         bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
     }
 
@@ -46,48 +68,38 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
             Fragment selectedFragment = null;
+            String tag = null;
 
             switch (menuItem.getItemId()) {
                 case R.id.tab_home:
-                    if (fragmentHome == null) {
-                        fragmentHome = new FragmentHome();
-                        fragmentTransaction.add(R.id.ly_home, fragmentHome, "home");
-                    }
                     selectedFragment = fragmentHome;
+                    tag = "home";
                     break;
                 case R.id.tab_find:
-                    if (fragmentSearch == null) {
-                        fragmentSearch = new FragmentSearch();
-                        fragmentTransaction.add(R.id.ly_home, fragmentSearch, "search");
-                    }
                     selectedFragment = fragmentSearch;
+                    tag = "search";
                     break;
                 case R.id.tab_history:
                     // 필요시 FragmentHistory 추가
                     break;
                 case R.id.tab_setting:
-                    if (fragmentSetting == null) {
-                        fragmentSetting = new FragmentSetting();
-                        fragmentTransaction.add(R.id.ly_home, fragmentSetting, "setting");
-                    }
                     selectedFragment = fragmentSetting;
+                    tag = "setting";
                     break;
             }
-            if (selectedFragment != null) {
-                for (Fragment fragment : fragmentManager.getFragments()) {
-                    if (fragment == selectedFragment) {
-                        fragmentTransaction.show(fragment);
-                    } else {
-                        fragmentTransaction.hide(fragment);
-                    }
-                }
-                fragmentTransaction.commit();
+
+            if (selectedFragment != null && tag != null) {
+                replaceFragment(selectedFragment, tag);
                 return true;
             }
             return false;
         }
+    }
+
+    private void replaceFragment(Fragment fragment, String tag) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.ly_home, fragment, tag);
+        fragmentTransaction.commit();
     }
 }
