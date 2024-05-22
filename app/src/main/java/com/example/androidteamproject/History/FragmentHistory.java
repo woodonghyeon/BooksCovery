88e33cfe7f7672a399b1424b27d014f6fe16f325
Mo yuseung <ahdbtmd147@naver.com>
2024-05-22 23:08:41 +0900

    history fragment DB 만드는 중..

--- app/src/main/java/com/example/androidteamproject/History/FragmentHistory.java
+++ app/src/main/java/com/example/androidteamproject/History/FragmentHistory.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.androidteamproject.R;
-import com.example.androidteamproject.Search.FragmentSearch;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class FragmentHistory extends Fragment {
 
@@ -19,6 +27,14 @@ public class FragmentHistory extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+    protected List<String> bookImageURL = new ArrayList<>();
+    private List<String> bookname = new ArrayList<>();
+    private List<String> authors = new ArrayList<>();
+    private List<String> publisher = new ArrayList<>();
+    private List<String> searchDate = new ArrayList<>();
+
+    private ListView list;
 
     public static FragmentHistory newInstance(String param1, String param2) {
         FragmentHistory fragment = new FragmentHistory();
@@ -41,11 +57,57 @@ public class FragmentHistory extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+        list = view.findViewById(R.id.List);
 
-
-
-        return super.onCreateView(inflater, container, savedInstanceState);
+        CustomList adapter = new CustomList(this, bookname, bookImageURL, authors, publisher, searchDate);
+        list.setAdapter(adapter);
+        return view;
     }
+
+    public class CustomList extends ArrayAdapter<String>{
+        private final FragmentHistory context;
+        private final List<String> bookNames;
+        private final List<String> bookImages;
+        private final List<String> bookAuthors;
+        private final List<String> bookPublishers;
+        private final List<String> bookSearchDates;
+
+        public CustomList(FragmentHistory context, List<String> bookNames, List<String> bookImages, List<String> bookAuthors, List<String> bookPublishers, List<String> bookSearchDates) {
+            super(context.getActivity(), R.layout.history_item, bookNames);
+            this.context = context;
+            this.bookNames = bookNames;
+            this.bookImages = bookImages;
+            this.bookAuthors = bookAuthors;
+            this.bookPublishers = bookPublishers;
+            this.bookSearchDates = bookSearchDates;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = context.getLayoutInflater();
+                view = inflater.inflate(R.layout.history_item, parent, false);
+            }
+
+            TextView nameView = view.findViewById(R.id.book_name);
+            TextView authorView = view.findViewById(R.id.authors);
+            TextView publisherView = view.findViewById(R.id.publisher);
+            //TextView dateView = view.findViewById(R.id.search_date);
+            ImageView imageView = view.findViewById(R.id.book_image);
+
+            nameView.setText(bookNames.get(position));
+            authorView.setText(bookAuthors.get(position));
+            publisherView.setText(bookPublishers.get(position));
+            //dateView.setText(bookSearchDates.get(position));
+            Picasso.get().load(bookImages.get(position)).into(imageView);
+
+            return view;
+        }
+    }
+
+
+
 }
