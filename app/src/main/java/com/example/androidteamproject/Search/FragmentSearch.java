4d18f6b531f8bf65177cf5d4f8ba7f580ce6ed05
parkminuk <qkralsdnr0203@naver.com>
2024-05-24 04:50:26 +0900

    UPDATE : 상단바 키워드 검색 기능 구현

--- app/src/main/java/com/example/androidteamproject/Search/FragmentSearch.java
+++ app/src/main/java/com/example/androidteamproject/Search/FragmentSearch.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -21,11 +20,10 @@ import com.google.android.material.chip.ChipGroup;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import androidx.appcompat.widget.SearchView;
 
 public class FragmentSearch extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -34,11 +32,11 @@ public class FragmentSearch extends Fragment {
     private String mParam2;
     private ViewPager2 mPager;
     private FragmentStateAdapter searchPagerAdapter;
-    private static List<String> keywords; //상수로 변경
-    private List<String> search_word = new ArrayList<>(Arrays.asList("테스트용 검색어1" , "테스트용 검색어2" , "테스트용 검색어3" , "테스트용 검색어4" , "테스트용 검색어5" , "테스트용 검색어6" , "테스트용 검색어7" , "테스트용 검색어8" , "테스트용 검색어9" , "테스트용 검색어10"));
+    private static List<String> keywords;
     @SuppressLint("SimpleDateFormat")
-    private static LocalDate mDate = LocalDate.now(); //현재 시각 (static 부여)
-    private static LocalDate checkDate; // 기록된 시각 (static 부여)
+    private static LocalDate mDate = LocalDate.now();
+    private static LocalDate checkDate;
+    private SearchView svKeyword;
 
     public FragmentSearch() {
     }
@@ -63,7 +61,25 @@ public class FragmentSearch extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // LayoutInflater를 이용하여 fragment_search 레이아웃을 뷰로 변환
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // sv_keyword SearchView를 초기화하고 리스너 설정
+        svKeyword = view.findViewById(R.id.sv_keyword);
+        svKeyword.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // 키워드 입력 후 검색 버튼을 누르면 searchByKeyword 메서드 호출
+                searchByKeyword(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // 텍스트가 변경될 때마다 호출됨 (여기서는 처리하지 않음)
+                return false;
+            }
+        });
         return view;
     }
 
@@ -71,19 +87,21 @@ public class FragmentSearch extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // SharedPreferences에서 keywords 불러오기
+        // SharedPreferences에서 키워드를 불러옴
         keywords = loadKeywordsFromSharedPreferences();
 
-        if (keywords.isEmpty() || timeCheck()) { // 키워드가 비어있거나 시간이 지난 경우에만 API 호출
-            getResponseApiKeyword(); // API 응답 데이터 가져오기
+        // 키워드가 비어있거나 시간이 지난 경우 API 호출
+        if (keywords.isEmpty() || timeCheck()) {
+            getResponseApiKeyword();
         } else {
-            addChips(); // SharedPreferences에서 불러온 키워드를 사용하여 칩 추가
+            // SharedPreferences에서 불러온 키워드를 사용하여 칩 추가
+            addChips();
         }
-
+        // 최근 많이 대출된 도서 이미지 출력
         getResponseApiLoanItems();
     }
 
-
+    // 키워드 검색 API 호출 메서드
     private void getResponseApiKeyword() {
         HttpConnection.getInstance(getContext()).getKeyword("json", new HttpConnection.HttpResponseCallback() {
             @Override
@@ -94,8 +112,9 @@ public class FragmentSearch extends Fragment {
                             JSONObject json = new JSONObject(responseData.toString());
                             JSONObject responseObject = json.getJSONObject("response");
                             JSONArray keywordsArray = responseObject.getJSONArray("keywords");
-                            keywords = new ArrayList<>(); // 키워드 초기화
+                            keywords = new ArrayList<>();
 
+                            // API 응답에서 키워드를 추출하여 리스트에 추가
                             for (int i = 0; i < keywordsArray.length(); i++) {
                                 JSONObject keywordObject = keywordsArray.getJSONObject(i);
                                 JSONObject keyword = keywordObject.getJSONObject("keyword");
@@ -103,10 +122,10 @@ public class FragmentSearch extends Fragment {
                                 keywords.add(word);
                             }
 
-                            // API 응답을 받아온 후 키워드를 SharedPreferences에 저장
+                            // 추출한 키워드를 SharedPreferences에 저장
                             saveKeywordsToSharedPreferences(keywords);
 
-                            // 키워드를 가져온 후 칩 추가
+                            // 키워드를 사용하여 칩 추가
                             addChips();
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -126,49 +145,33 @@ public class FragmentSearch extends Fragment {
         });
     }
 
+    // 키워드를 사용하여 칩을 추가하는 메서드
     private void addChips() {
         if (getContext() == null) return;
         ChipGroup chipGroup1 = requireView().findViewById(R.id.chip_group1);
-        ChipGroup chipGroup2 = requireView().findViewById(R.id.chip_group2);
 
-        // 다른 이벤트가 발생하면 중복적으로 chip이 발생하여 일부러 한번 지우고 다시 세팅하는걸로 넣은거임
+        // 중복 칩 제거를 위해 기존 칩들을 모두 제거
         chipGroup1.removeAllViews();
-        chipGroup2.removeAllViews();
 
         // 키워드를 이용하여 칩 추가
         for (String keyword : keywords) {
             Chip chip = new Chip(requireContext());
             chip.setText(keyword);
-            chip.setChipBackgroundColorResource(R.color.brandcolor1); // 칩 배경 색 설정
-            chip.setTextColor(getResources().getColor(R.color.primaryDarkColor)); // 텍스트 색상 설정
-            chip.setChipStrokeColorResource(R.color.primaryDarkColor); // 칩 테두리 색상 설정
-            chip.setChipStrokeWidth(1.0f); // 칩 테두리 두께 설정
+            chip.setChipBackgroundColorResource(R.color.brandcolor1);
+            chip.setTextColor(getResources().getColor(R.color.primaryDarkColor));
+            chip.setChipStrokeColorResource(R.color.primaryDarkColor);
+            chip.setChipStrokeWidth(1.0f);
 
             // 칩 클릭 이벤트 추가
             chip.setOnClickListener(v -> onChipClick(keyword));
 
-            chipGroup1.addView(chip); // 칩을 ChipGroup에 추가
-        }
-
-        // 검색어를 이용하여 칩 추가 (나중에 DB에서 검색어를 가져와야함)
-        for (String word : search_word) {
-            Chip chip = new Chip(requireContext());
-            chip.setText(word);
-            chip.setChipBackgroundColorResource(R.color.brandcolor1); // 칩 배경 색 설정
-            chip.setTextColor(getResources().getColor(R.color.primaryDarkColor)); // 텍스트 색상 설정
-            chip.setChipStrokeColorResource(R.color.primaryDarkColor); // 칩 테두리 색상 설정
-            chip.setChipStrokeWidth(1.0f); // 칩 테두리 두께 설정
-
-            // 칩 클릭 이벤트 추가
-            chip.setOnClickListener(v -> onChipClick(word));
-
-            chipGroup2.addView(chip); // 칩을 ChipGroup에 추가
+            chipGroup1.addView(chip);
         }
     }
 
     // 칩 클릭 이벤트 처리 메서드
     private void onChipClick(String keyword) {
-        FragmentKeywordSearch fragment = FragmentKeywordSearch.newInstance(keyword, ""); // 두 번째 인자는 필요에 따라 변경
+        FragmentKeywordSearch fragment = FragmentKeywordSearch.newInstance(keyword, "");
 
         FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
 
@@ -184,16 +187,15 @@ public class FragmentSearch extends Fragment {
         transaction.commit();
     }
 
-
-    // 최근 많이 검색된 도서 이미지 출력 (현재는 많이 대출된 도서로 출력함 -> 수정 예정)
+    // 최근 많이 대출된 도서 이미지 출력
     private void getResponseApiLoanItems() {
-        String startDt = "2023-01-01"; // 시작 날짜 (예시)
-        String endDt = "2024-05-01"; // 종료 날짜 (예시)
-        String from_age = "20"; //최소 나이
-        String to_age = "40"; // 최대 나이
-        int pageNo = 1; // 페이지 번호 (예시)
-        int pageSize = 10; // 페이지 크기 (예시)
-        String format = "json"; // 응답 형식 (예시)
+        String startDt = "2023-01-01";
+        String endDt = "2024-05-01";
+        String from_age = "20";
+        String to_age = "40";
+        int pageNo = 1;
+        int pageSize = 10;
+        String format = "json";
 
         HttpConnection.getInstance(getContext()).getLoanItems(startDt, endDt, from_age, to_age, pageNo, pageSize, format, new HttpConnection.HttpResponseCallback<List<SearchBook>>() {
             @Override
@@ -206,7 +208,6 @@ public class FragmentSearch extends Fragment {
                             imageUrls.add(book.getBookImageUrl());
                             bookName.add(book.getBookName());
                         }
-                        // ViewPager2에 이미지 추가
                         setupViewPager(bookName, imageUrls);
                         Log.d("API Response", "Image URLs: " + imageUrls.toString() + ", BookName: " + bookName.toString());
                     });
@@ -217,7 +218,6 @@ public class FragmentSearch extends Fragment {
             public void onFailure(Exception e) {
                 if (getActivity() != null) {
                     getActivity().runOnUiThread(() -> {
-                        // 에러 처리 로직 추가
                         Log.e("API Failure", "Error: " + e.getMessage());
                     });
                 }
@@ -225,13 +225,11 @@ public class FragmentSearch extends Fragment {
         });
     }
 
-    // ViewPager2 Setting
+    // ViewPager2 설정 메서드
     private void setupViewPager(List<String> bookName, List<String> imageUrls) {
         if (imageUrls == null || imageUrls.isEmpty()) {
-            // 이미지 URL이 없는 경우 처리
             return;
         }
-        // 이미지 URL이 있을 경우 뷰페이저 설정
         if (getView() == null) {
             return;
         }
@@ -244,78 +242,86 @@ public class FragmentSearch extends Fragment {
         int startPos = imageUrls.size() / 2;
         mPager.setCurrentItem(startPos);
 
-        // viewpager2 간격 변환을 위함 -> res.values.dimes.xml에서 확인
         int pageMarginPx = getResources().getDimensionPixelOffset(R.dimen.searchPageMargin);
         int pagerWidth = getResources().getDimensionPixelOffset(R.dimen.searchPageWidth);
         int screenWidth = getResources().getDisplayMetrics().widthPixels;
         int offsetPx = screenWidth - pageMarginPx - pagerWidth;
 
-        // viewpager2 간격 변환
         mPager.setPageTransformer((page, position) -> page.setTranslationX(position * -offsetPx));
-
-        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-                if (positionOffsetPixels == 0) {
-                    mPager.setCurrentItem(position);
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-            }
-        });
+        mPager.setCurrentItem(mPager.getAdapter().getItemCount() / 2);
     }
 
-    private boolean timeCheck(){
-        //간단 설명
-        //기록된 시간, 현재시간을 비교해서 기록된 시간보다 현재시간이 하루 이상 많을 때 api요청
-        //만약 아니라면 아직 하루가 안지났기 때문에 최신 데이터임
-        // mDate가 현재 시각, checkDate가 기록된 시간
-        try{
-            int check = mDate.compareTo(checkDate); //시간 비교
+    // SharedPreferences에서 키워드를 불러오는 메서드
+    private List<String> loadKeywordsFromSharedPreferences() {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("keywords", Context.MODE_PRIVATE);
+        String keywordsString = sharedPreferences.getString("keywords", "");
+        List<String> keywordsList = new ArrayList<>();
 
-            //현재 시간이 기록된 시간보다 높다 => 기록된 키워드는 예전 데이터이다. => API요청해야한다.
-            if(check > 0){ return true; }
-            else { return false; }
+        if (!keywordsString.isEmpty()) {
+            String[] keywordsArray = keywordsString.split(",");
+            for (String keyword : keywordsArray) {
+                keywordsList.add(keyword);
+            }
         }
-        //만약에 checkDate가 비어있다 => 처음 들어왔다 => API요청해야한다.
-        catch (NullPointerException ignored){
-            checkDate = mDate; //기록을 현재 시간으로 교체 => 왜냐하면 지금 요청 할꺼니까?
+        return keywordsList;
+    }
+
+    // SharedPreferences에 키워드를 저장하는 메서드
+    private void saveKeywordsToSharedPreferences(List<String> keywords) {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("keywords", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        StringBuilder keywordsString = new StringBuilder();
+
+        for (int i = 0; i < keywords.size(); i++) {
+            keywordsString.append(keywords.get(i));
+            if (i < keywords.size() - 1) {
+                keywordsString.append(",");
+            }
+        }
+
+        editor.putString("keywords", keywordsString.toString());
+        editor.apply();
+    }
+
+    // 시간을 체크하여 일정 시간이 지났는지 확인하는 메서드
+    private boolean timeCheck() {
+        // 간단 설명: 기록된 시간, 현재 시간을 비교해서 기록된 시간보다 현재 시간이 하루 이상 많을 때 API 요청
+        // 만약 아니라면 아직 하루가 안 지났기 때문에 최신 데이터임
+        // mDate가 현재 시각, checkDate가 기록된 시간
+        try {
+            int check = mDate.compareTo(checkDate); // 시간 비교
+
+            // 현재 시간이 기록된 시간보다 높다 => 기록된 키워드는 예전 데이터이다. => API 요청해야 한다.
+            if (check > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        // 만약에 checkDate가 비어있다 => 처음 들어왔다 => API 요청해야 한다.
+        catch (NullPointerException ignored) {
+            checkDate = mDate; // 기록을 현재 시간으로 교체
             return true;
         }
     }
 
-    private void saveKeywordsToSharedPreferences(List<String> keywords) {
-        SharedPreferences sharedPreferences = getContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+    // 키워드 검색 기능을 위한 메서드 추가
+    private void searchByKeyword(String keyword) {
+        // FragmentKeywordSearch를 생성하고 키워드를 전달
+        FragmentKeywordSearch fragment = FragmentKeywordSearch.newInstance(keyword, "");
 
-        // List<String>를 JSON 문자열로 변환
-        JSONArray jsonArray = new JSONArray(keywords);
-        String jsonString = jsonArray.toString();
+        // FragmentTransaction을 사용하여 프래그먼트를 추가 또는 교체
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
 
-        editor.putString("keywords", jsonString);
-        editor.apply();
-    }
-
-    private List<String> loadKeywordsFromSharedPreferences() {
-        SharedPreferences sharedPreferences = getContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
-        String jsonString = sharedPreferences.getString("keywords", null);
-
-        if (jsonString != null) {
-            try {
-                JSONArray jsonArray = new JSONArray(jsonString);
-                List<String> keywords = new ArrayList<>();
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    keywords.add(jsonArray.getString(i));
-                }
-                return keywords;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        // 현재 프래그먼트를 가져와서 숨김
+        Fragment currentFragment = getParentFragmentManager().findFragmentById(R.id.ly_home);
+        if (currentFragment != null) {
+            transaction.hide(currentFragment);
         }
-        return new ArrayList<>(); // 저장된 키워드가 없으면 빈 리스트 반환
+
+        // 새 프래그먼트를 추가
+        transaction.add(R.id.ly_home, fragment);
+        transaction.addToBackStack(null); // 백스택에 추가하여 뒤로가기 버튼을 눌렀을 때 이전 프래그먼트로 돌아갈 수 있음
+        transaction.commit();
     }
 }
