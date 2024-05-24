d3b0df60dd5c19d4f51d0ee9dba95615d49202a1
parkminuk <qkralsdnr0203@naver.com>
2024-05-25 01:44:29 +0900

    UPDATE : isbn 테스트중

--- app/src/main/java/com/example/androidteamproject/ApiData/SearchBookTitle.java
+++ app/src/main/java/com/example/androidteamproject/ApiData/SearchBookTitle.java
@@ -1,13 +1,18 @@
 package com.example.androidteamproject.ApiData;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.util.Log;
+
 public class SearchBookTitle {
-    private String isbn ,bookName, bookImageUrl, authors, publisher, publication_year;
+    private String isbn, bookName, bookImageUrl, authors, publisher, publication_year;
 
     public SearchBookTitle() {
     }
 
     public SearchBookTitle(String isbn, String bookName, String authors, String bookImageUrl, String publisher, String publication_year) {
         this.isbn = isbn;
+        Log.d(TAG, "ISBN: " + isbn); // 테스트 중 나중에 지우면 됨
         this.bookName = bookName;
         this.bookImageUrl = bookImageUrl;
         this.authors = authors;
