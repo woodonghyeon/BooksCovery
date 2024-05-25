9b4e3e99675023dc2d768c68c6331138ec28db1f
woodonghyeon <silhone__@naver.com>
2024-05-25 16:06:31 +0900

    이번주 인기도서 이미지 클릭시 상세보기 출력(수정중)

--- app/src/main/java/com/example/androidteamproject/Home/WeekBookImageFragment.java
+++ app/src/main/java/com/example/androidteamproject/Home/WeekBookImageFragment.java
@@ -14,25 +14,31 @@ import androidx.fragment.app.Fragment;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
 import com.example.androidteamproject.R;
+import com.squareup.picasso.Picasso;
 
 public class WeekBookImageFragment extends Fragment {
     private static final String ARG_IMAGE_URL = "imageUrl";
     private static final String ARG_BOOKNAME = "bookName";
     private static final String ARG_AUTHORS = "authors";
     private static final String ARG_CLASS_NM = "class_nm";
+    private static final String ARG_ISBN13 = "isbn13";
     private String imageUrl;
     private String bookName;
     private String authors;
     private String class_nm;
+    private String isbn13;
+    private WeekBookAdapter.OnItemClickListener onItemClickListener;
 
-    public static WeekBookImageFragment newInstance(String class_nm, String bookName, String authors, String imageUrl) {
+    public static WeekBookImageFragment newInstance(String class_nm, String bookName, String authors, String imageUrl, String isbn13, WeekBookAdapter.OnItemClickListener onItemClickListener) {
         WeekBookImageFragment fragment = new WeekBookImageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_IMAGE_URL, imageUrl);
         args.putString(ARG_BOOKNAME, bookName);
         args.putString(ARG_AUTHORS, authors);
         args.putString(ARG_CLASS_NM, class_nm);
+        args.putString(ARG_ISBN13, isbn13);
         fragment.setArguments(args);
+        fragment.setOnItemClickListener(onItemClickListener);
         return fragment;
     }
 
@@ -44,6 +50,7 @@ public class WeekBookImageFragment extends Fragment {
             bookName = getArguments().getString(ARG_BOOKNAME);
             authors = getArguments().getString(ARG_AUTHORS);
             class_nm = getArguments().getString(ARG_CLASS_NM);
+            isbn13 = getArguments().getString(ARG_ISBN13);
         }
     }
 
@@ -56,11 +63,29 @@ public class WeekBookImageFragment extends Fragment {
         TextView weekBookAuthor = view.findViewById(R.id.tv_weekBookAuthor);
         TextView weekBookClassNm = view.findViewById(R.id.tv_weekBookClassName);
 
-        // Glide를 사용하여 이미지 로드
-        Glide.with(this).load(imageUrl).override(Target.SIZE_ORIGINAL).error(R.drawable.ic_error).into(weekBookImg);
+        if (getArguments() != null) {
+            class_nm = getArguments().getString(ARG_CLASS_NM);
+            bookName = getArguments().getString(ARG_BOOKNAME);
+            authors = getArguments().getString(ARG_AUTHORS);
+            imageUrl = getArguments().getString(ARG_IMAGE_URL);
+            isbn13 = getArguments().getString(ARG_ISBN13);
+        }
+
+        Picasso.get().load(imageUrl).into(weekBookImg);
+
+        weekBookImg.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(isbn13, bookName, authors, imageUrl);
+            }
+        });
+
         weekBookTitle.setText(bookName);
         weekBookAuthor.setText(authors);
         weekBookClassNm.setText(class_nm);
         return view;
+    }
+
+    public void setOnItemClickListener(WeekBookAdapter.OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 }
