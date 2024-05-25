09915cba7ba78df65c43f14ec8a1330c784a9ba1
woodonghyeon <silhone__@naver.com>
2024-05-26 00:54:11 +0900

    API 8번 수정중 (상세페이지 출력 테스트)

--- app/src/main/java/com/example/androidteamproject/Home/FragmentBookDetail.java
+++ app/src/main/java/com/example/androidteamproject/Home/FragmentBookDetail.java
@@ -108,23 +108,57 @@ public class FragmentBookDetail extends Fragment {
                     getActivity().runOnUiThread(() -> {
                         // 로그 추가하여 데이터 확인
                         System.out.println("Book Detail: " + bookDetail.toString());
-                        // 모든 데이터를 설정
+
+                        // 모든 데이터를 설정하기 전에 각 데이터에 로그를 추가하여 확인
+                        System.out.println("Setting bookName: " + bookDetail.getBookName());
                         bookNameTextView.setText(bookDetail.getBookName());
+
+                        System.out.println("Setting authors: " + bookDetail.getAuthors());
                         authorsTextView.setText(bookDetail.getAuthors());
+
+                        System.out.println("Setting description: " + bookDetail.getDescription());
                         descriptionTextView.setText(bookDetail.getDescription());
+
+                        System.out.println("Setting publisher: " + bookDetail.getPublisher());
                         publisherTextView.setText(bookDetail.getPublisher());
+
+                        System.out.println("Setting publication_year: " + bookDetail.getPublication_year());
                         publicationYearTextView.setText(bookDetail.getPublication_year());
+
+                        System.out.println("Setting class_no: " + bookDetail.getClass_no());
                         classNoTextView.setText(bookDetail.getClass_no());
+
+                        System.out.println("Setting class_nm: " + bookDetail.getClass_nm());
                         classNmTextView.setText(bookDetail.getClass_nm());
+
+                        System.out.println("Setting loanCnt: " + bookDetail.getLoanCnt());
                         loanCntTextView.setText(bookDetail.getLoanCnt());
+
+                        System.out.println("Setting month: " + bookDetail.getMonth());
                         monthTextView.setText(bookDetail.getMonth());
+
+                        System.out.println("Setting loanHistoryCnt: " + bookDetail.getLoanHistoryCnt());
                         loanHistoryCntTextView.setText(bookDetail.getLoanHistoryCnt());
+
+                        System.out.println("Setting ranking: " + bookDetail.getRanking());
                         rankingTextView.setText(bookDetail.getRanking());
+
+                        System.out.println("Setting age: " + bookDetail.getAge());
                         ageTextView.setText(bookDetail.getAge());
+
+                        System.out.println("Setting gender: " + bookDetail.getGender());
                         genderTextView.setText(bookDetail.getGender());
+
+                        System.out.println("Setting loanGrpsCnt: " + bookDetail.getLoanGrpsCnt());
                         loanGrpsCntTextView.setText(bookDetail.getLoanGrpsCnt());
+
+                        System.out.println("Setting loanGrpsRanking: " + bookDetail.getLoanGrpsRanking());
                         loanGrpsRankingTextView.setText(bookDetail.getLoanGrpsRanking());
+
+                        System.out.println("Setting word: " + bookDetail.getWord());
                         wordTextView.setText(bookDetail.getWord());
+
+                        System.out.println("Setting weight: " + bookDetail.getWeight());
                         weightTextView.setText(bookDetail.getWeight());
 
                         // 수정: 추가된 이미지 로딩 로직
