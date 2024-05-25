3770f9d4b193cd84eaaaba1ae6a87ebf0cc135c6
parkminuk <qkralsdnr0203@naver.com>
2024-05-25 17:42:03 +0900

    UPDATE : 리스트뷰 클릭 이벤트

--- app/src/main/java/com/example/androidteamproject/Search/FragmentTitleSearch.java
+++ app/src/main/java/com/example/androidteamproject/Search/FragmentTitleSearch.java
@@ -5,13 +5,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.androidteamproject.ApiData.HttpConnection;
-import com.example.androidteamproject.ApiData.SearchBookKeyword;
 import com.example.androidteamproject.ApiData.SearchBookTitle;
 import com.example.androidteamproject.R;
 
@@ -29,7 +30,7 @@ public class FragmentTitleSearch extends Fragment {
     private List<SearchBookTitle> bookList;
 
     public FragmentTitleSearch() {
-        // Required empty public constructor
+        // 기본 생성자
     }
 
     public static FragmentTitleSearch newInstance(String param1, String param2) {
@@ -66,10 +67,28 @@ public class FragmentTitleSearch extends Fragment {
         adapter = new BookListTitleAdapter(getContext(), bookList);
         listView.setAdapter(adapter);
 
+        // 리스트뷰 항목 클릭 리스너 설정
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SearchBookTitle selectedBook = bookList.get(position);
+                showBookDetail(selectedBook);
+            }
+        });
+
         // API 데이터 가져오기
         getResponseApiBookSearch();
 
         return view;
+    }
+
+    private void showBookDetail(SearchBookTitle book) {
+        FragmentBookDetail fragment = FragmentBookDetail.newInstance(book);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.ly_home, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     private void getResponseApiBookSearch() {
