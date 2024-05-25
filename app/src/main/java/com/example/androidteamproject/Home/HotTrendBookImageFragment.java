efa602795846b0909f9ca57d6fe0534d4e781ea8
woodonghyeon <silhone__@naver.com>
2024-05-26 02:35:43 +0900

    API 8번 수정 (연결작업중)

--- app/src/main/java/com/example/androidteamproject/Home/HotTrendBookImageFragment.java
+++ app/src/main/java/com/example/androidteamproject/Home/HotTrendBookImageFragment.java
@@ -14,25 +14,31 @@ import androidx.fragment.app.Fragment;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
 import com.example.androidteamproject.R;
+import com.squareup.picasso.Picasso;
 
 public class HotTrendBookImageFragment extends Fragment {
     private static final String ARG_IMAGE_URL = "imageUrl";
     private static final String ARG_BOOKNAME = "bookName";
     private static final String ARG_AUTHORS = "authors";
     private static final String ARG_CLASS_NM = "class_nm";
+    private static final String ARG_ISBN13 = "isbn13";
     private String imageUrl;
     private String bookName;
     private String authors;
     private String class_nm;
+    private String isbn13;
+    private HotTrendBookAdapter.OnItemClickListener onItemClickListener;
 
-    public static HotTrendBookImageFragment newInstance(String class_nm, String bookName, String authors, String imageUrl) {
+    public static HotTrendBookImageFragment newInstance(String class_nm, String bookName, String authors, String imageUrl, String isbn13, HotTrendBookAdapter.OnItemClickListener onItemClickListener) {
         HotTrendBookImageFragment fragment = new HotTrendBookImageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_IMAGE_URL, imageUrl);
         args.putString(ARG_BOOKNAME, bookName);
         args.putString(ARG_AUTHORS, authors);
         args.putString(ARG_CLASS_NM, class_nm);
+        args.putString(ARG_ISBN13, isbn13);
         fragment.setArguments(args);
+        fragment.setOnItemClickListener(onItemClickListener);
         return fragment;
     }
 
@@ -44,6 +50,7 @@ public class HotTrendBookImageFragment extends Fragment {
             bookName = getArguments().getString(ARG_BOOKNAME);
             authors = getArguments().getString(ARG_AUTHORS);
             class_nm = getArguments().getString(ARG_CLASS_NM);
+            isbn13 = getArguments().getString(ARG_ISBN13);
         }
     }
 
@@ -56,12 +63,30 @@ public class HotTrendBookImageFragment extends Fragment {
         TextView hotTrendBookAuthor = view.findViewById(R.id.tv_hotTrendBookAuthor);
         TextView hotTrendClassNm = view.findViewById(R.id.tv_hotTrendClassName);
 
-        // Glide를 사용하여 이미지 로드
-        Glide.with(this).load(imageUrl).error(R.drawable.ic_error).into(hotTrendBookImg);
+        if (getArguments() != null) {
+            class_nm = getArguments().getString(ARG_CLASS_NM);
+            bookName = getArguments().getString(ARG_BOOKNAME);
+            authors = getArguments().getString(ARG_AUTHORS);
+            imageUrl = getArguments().getString(ARG_IMAGE_URL);
+            isbn13 = getArguments().getString(ARG_ISBN13);
+        }
+
+        Picasso.get().load(imageUrl).into(hotTrendBookImg);
+
+        hotTrendBookImg.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(isbn13, bookName, authors, imageUrl);
+            }
+        });
+
         hotTrendBookTitle.setText(bookName);
         hotTrendBookAuthor.setText(authors);
         hotTrendClassNm.setText(class_nm);
 
         return view;
+    }
+
+    public void setOnItemClickListener(HotTrendBookAdapter.OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 }
