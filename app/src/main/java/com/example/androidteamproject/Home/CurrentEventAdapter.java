cea43628e424715984090602f2ea771beff55f27
WooDongHyeon <silhone__@naver.com>
2024-07-01 17:15:01 +0900

    CurrentEvent 수정(lib.yju.ac.kr 최신 게시글 이미지 스크랩하여 사용, 10개)

--- app/src/main/java/com/example/androidteamproject/Home/CurrentEventAdapter.java
+++ app/src/main/java/com/example/androidteamproject/Home/CurrentEventAdapter.java
@@ -5,40 +5,33 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class CurrentEventAdapter extends FragmentStateAdapter {
-    private final int[] imageResIds;
-    private final int imageCount;
+import java.util.List;
 
-    public CurrentEventAdapter(FragmentActivity fa, int count) {
+public class CurrentEventAdapter extends FragmentStateAdapter {
+    private final List<String> imageUrls;
+
+    public CurrentEventAdapter(FragmentActivity fa, List<String> imageUrls) {
         super(fa);
-        imageCount = count;
-        // 이미지 리소스 ID 배열 초기화
-        imageResIds = new int[imageCount];
-        // 이미지 리소스 ID 배열에 각 이미지에 해당하는 리소스 ID 저장
-        for (int i = 0; i < imageCount; i++) {
-            // 이미지 파일 이름이 순차적으로 설정되어 있다면, 리소스 ID를 계산하여 배열에 저장
-            String imageName = "current_event_" + (i + 1); // 예: test1, test2, test3
-            imageResIds[i] = fa.getResources().getIdentifier(imageName, "drawable", fa.getPackageName());
-        }
+        this.imageUrls = imageUrls;
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        // 이미지 리소스 ID 배열에서 현재 페이지에 해당하는 이미지의 리소스 ID를 가져와서 Fragment를 생성하여 반환
+        // 이미지 URL 배열에서 현재 페이지에 해당하는 URL을 가져와서 Fragment를 생성하여 반환
         int index = getRealPosition(position);
-        return CurrentEventImageFragment.newInstance(imageResIds[index]);
+        return CurrentEventImageFragment.newInstance(imageUrls.get(index));
     }
 
     @Override
     public int getItemCount() {
-        // 페이지 수는 이미지의 개수와 동일하게 설정
+        // 페이지 수는 이미지 URL의 개수와 동일하게 설정
         // 이미지 개수의 배수로 설정하여 무한 스크롤 가능하도록 함
-        return imageCount * 1000; // 1000은 임의로 설정한 값
+        return imageUrls.size() * 1000; // 1000은 임의로 설정한 값
     }
 
     public int getRealPosition(int position) {
         // 무한 스크롤을 위해 실제 위치를 계산하여 반환
-        return position % imageCount;
+        return position % imageUrls.size();
     }
 }
